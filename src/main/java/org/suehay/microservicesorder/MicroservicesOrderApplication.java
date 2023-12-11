679cf112d9be84a473c6128379ef008ad2c47813
palmerodev <victor.palmero@undostres.com.mx>
2023-12-11 01:04:29 -0500

    create microservices order

---
+++ src/main/java/org/suehay/microservicesorder/MicroservicesOrderApplication.java
@@ -0,0 +1,13 @@
+package org.suehay.microservicesorder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroservicesOrderApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MicroservicesOrderApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/suehay/microservicesorder/MicroservicesOrderApplicationTests.java
@@ -0,0 +1,13 @@
+package org.suehay.microservicesorder;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroservicesOrderApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
